c4f7ca07af020c2bccf308f01efcee23d7c6d154
Adam Of Venn <46652493+AdamVenn@users.noreply.github.com>
2025-06-30 15:22:27 -0400

    Remove credit in footer

--- inc/storefront-template-hooks.php
+++ inc/storefront-template-hooks.php
@@ -35,10 +35,8 @@ add_action( 'storefront_header', 'storefront_primary_navigation_wrapper_close', 
  * Footer
  *
  * @see  storefront_footer_widgets()
- * @see  storefront_credit()
  */
 add_action( 'storefront_footer', 'storefront_footer_widgets', 10 );
-add_action( 'storefront_footer', 'storefront_credit', 20 );
 
 /**
  * Homepage
